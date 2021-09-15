b2a39a7ee3a4c037818a6aedff205dc5c9c289b9
Surfsol <surfsol@protonmail.com>
2021-09-15 17:49:42 -0300

    add navigation

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//https://reactnavigation.org/docs/params  passing parameters
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Direct TV</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Videos')}
+      />
     </View>
   );
 }
+
+function VideoScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', marginTop: 5 }}>
+      <Text>Video Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }}/>
+        <Stack.Screen name="Videos" component={VideoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
